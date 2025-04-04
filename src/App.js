03b4c3f52b0841a4c976e3ef4fa495f84ed25204
varshaD024 <varsha9824516@gmail.com>
2025-04-04 09:32:08 +0530

    Footer is Fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";  // Import the Footer component
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#F5F5DC]">
+    <div className="min-h-screen bg-[#F5F5DC] flex flex-col">
       <Navbar />
-      <main className="p-4 bg-[#F5F5DC]"> {/* Added the background class here */}
+      
+      <main className="flex-grow p-4 bg-[#F5F5DC]">  
         <h1 className="text-center text-4xl">Welcome to Mandd Sobhann</h1>
       </main>
+
+      <Footer />  {/* Add Footer at the bottom */}
     </div>
   );
 }
